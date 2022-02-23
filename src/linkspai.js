f31ee490de1de6915883738442e582036e182ed9
Cosme Sousa <36869385+Cosmess@users.noreply.github.com>
2022-02-22 21:28:06 -0300

    Refatorando

---
+++ src/linkspai.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const cheerio= require('cheerio');
+const { children, not } = require('cheerio/lib/api/traversing');
+
+const urlpai = 'https://www.gov.br/pt-br/noticias/ultimas-noticias?b_start:int=0';
+
+axios.get(urlpai)
+.then(resp=>{
+    const dadoshtml = resp.data;
+    const $ = cheerio.load(dadoshtml);
+    const dados = []
+    $('[class="summary url"]').each((i,e)=>{
+        const link = $(e).attr('href');
+       //console.log(link);
+       dados.push(link);
+    });
+    console.log(dados);
+});
